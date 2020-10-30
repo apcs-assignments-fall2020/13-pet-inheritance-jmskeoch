ebc0d8fba00609449c31eed477af8c8721633474
jmskeoch <jmskeoch@yahoo.com>
2020-10-30 00:54:58 +0000

    Implement dog class and methods

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,37 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public Dog() {
+        super("Max", 1);
+        this.breed = "Labrador Retriever";
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Woof!");
+    }
 
     // toString method
-
+    @Override
+    public String toString() {
+        String x = this.getName() + " , " + this.getAge() + " , " + this.breed;
+        return x;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        this.breed = (breed.trim().length() != 0) ? breed : this.breed;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,13 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog riley = new Dog("Riley", 10, "Black Labrador");
+        Dog max = new Dog();
+        System.out.println(riley);
+        System.out.println(max);
+        riley.setName("Jersey");
+        max.setAge(2);
+        System.out.println(riley.getName());
+        System.out.println(max.getAge());
+        System.out.println(riley.getBreed());
     }
 }
